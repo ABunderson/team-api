a12eaf5df73cd0b9ee9ffa867c26185aca5202c3
Nathanael Budge <nathanaelcbudge@gmail.com>
2023-05-31 11:12:26 -0600

    Added Nathanael to server.js

--- server.js
+++ server.js
@@ -8,4 +8,6 @@ app.get('/', (req,res) => {res.send("Hello Browser")});
 
 app.get('/sean', (req,res) => {res.send("Hello Sean")});
 
+app.get('/nathanael', (req,res) => {res.send("Hello Nathanael")});
+
 app.listen(port, ()=>console.log("Listening"));
