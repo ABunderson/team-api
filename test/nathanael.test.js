5aae413019e7a3db578cdd403379b4efa1b72bdb
Nathanael Budge <nathanaelcbudge@gmail.com>
2023-06-07 11:44:47 -0600

    added nathanael test file

---
+++ test/nathanael.test.js
@@ -0,0 +1,9 @@
+it("Should get a name", async ()=>{
+
+    const response = await fetch('http://localhost:3000/nathanael');
+
+    const name = await response.text();
+
+    expect(name).toBe("Hello Nathanael");
+
+})
