2f616d0bb553c417debb419d5dbaef9d547fcd2b
Trent Cooley <Cooley.trent@gmail.com>
2023-06-07 10:18:09 -0600

    added teh api-tests folder with tests

--- server.js
+++ server.js
@@ -6,12 +6,6 @@ const app = express();
 
 app.get('/', (req,res) => {res.send("Hello Browser")});
 
-app.get('/sean', (req,res) => {res.send("Hello Sean")});
-
-app.get('/nathanael', (req,res) => {res.send("Hello Nathanael")});
-
 app.get('/trent', (req,res) => {res.send("Hello Trent")});
-
-app.get('/adrienne', (req,res) => {res.send("Hello Adrienne")});
 
 app.listen(port, ()=>console.log("Listening"));
